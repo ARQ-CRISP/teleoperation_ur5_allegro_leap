d29e6c123a31e1ca89dd8c09eee06ade3cda8055
Claudio Coppola <claudiocoppola90@gmail.com>
2021-06-07 15:41:14 +0100

    cleanup

--- src/teleoperation_ur5_allegro_leap/teleop/__init__.py
+++ src/teleoperation_ur5_allegro_leap/teleop/__init__.py
@@ -7,8 +7,8 @@ import allegro.allegro_state as allegro_state
 import allegro.leap_state as leap_state
 import allegro.utils 
 import allegro.allegro_utils as allegro_utils
-import ur5.ur5_utils as ur5_utils
-import ur5.ur5_state as ur5_state
+# import ur5.ur5_utils as ur5_utils
+# import ur5.ur5_state as ur5_state
 
 __all__ = ['utils', 'allegro_utils', 'allegro_state', 'Leap_Teleop_Allegro',
            'Leap_Teleop_RelaxedIK', 'Relaxed_UR5_Controller', 'calibration']
